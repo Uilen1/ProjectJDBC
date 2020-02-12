252de55ccdfbbc5a5566eccb11b770beda0fce4d
Uilen Lelles <uilenlelles@hotmail.com>
2020-02-12 10:36:52 -0300

    Implementation the method findAll

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -128,8 +128,53 @@ public class SellerDaoJDBC implements SellerDao {
 			
 		}
 	}
-	
 
+	@Override
+	public List<Seller> findAll() {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name");
+			
+			rs = st.executeQuery();
+			List<Seller> list = new ArrayList<Seller>();
+			Map<Integer, Department> map = new HashMap<Integer, Department>();
+			
+			while(rs.next()) {
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {
+					
+					dep = instatiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+					
+				}
+				
+				Seller objSeller = instatiateSeller(rs, dep);
+				list.add(objSeller);
+				
+			}
+			
+			return list;
+			
+		} 
+		catch (SQLException e) {
+			throw new DbException("Error: " + e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
+	}
+	
 	private Seller instatiateSeller(ResultSet rs, Department dep) throws SQLException {
 		Seller objSeller = new Seller();
 		objSeller.setId(rs.getInt("Id"));
@@ -146,11 +191,5 @@ public class SellerDaoJDBC implements SellerDao {
 			dep.setId(rs.getInt("DepartmentId"));
 			dep.setName(rs.getString("DepName"));
 			return dep;
-	}
-
-	@Override
-	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 }
